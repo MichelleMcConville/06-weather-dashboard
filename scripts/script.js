cbec2221521333e79a1113011bf0b98823df5283
Michelle McConville <mchelmconvl@gmail.com>
2020-10-17 00:01:58 -0500

    fixing dynamic html for card deck

--- scripts/script.js
+++ scripts/script.js
@@ -1,96 +1,138 @@
 var date = moment().format("MM/DD/YYYY");
-var cities = [];
+var cities;
 var city = "";
 
 loadCity();
 
 //init function
 
-// Saving cities to local storage
-function saveCity() {
-  localStorage.setItem("lscity", JSON.stringify(cities));
-};
-
 // Getting cities from local storage
 function loadCity() {
   cities = JSON.parse(localStorage.getItem("lscity"));
-};
+  if (cities) {
+    console.log(cities[cities.length - 1]);
+    weatherData(cities[cities.length - 1])
+  } else {
+    cities = [];
+  }
+}
 
-// F(x) for displaying cities in sideNav
+//Dynamically building City List under search box
 function citySearchList() {
   $("#ul-view").empty();
 
   for (var i = 0; i < cities.length; i++) {
-    var newLi = $("<li>").attr("class","city-list").attr("city-name", cities[i]).text(cities[i]);
+    var newLi = $("<li>").attr("class", "city-list").attr("city-name", cities[i]).text(cities[i]);
     $("#ul-view").prepend(newLi);
-    console.log(newLi);
   }
 }
 
+// Saving cities to local storage
+function saveCity() {
+  localStorage.setItem("lscity", JSON.stringify(cities));
+}
+
 // F(x) for search button click event
-$("#searchBtn").on("click", function(event) {
+$("#searchBtn").on("click", function (event) {
   event.preventDefault();
   city = $("#addCity").val().trim();
+  console.log(city)
+  weatherData(city);
+});
+
+// Loads city on refresh only add cities if not in the list
+function weatherData(city) {
   $("#addCity").val("");
-  cities.push(city);
-  saveCity();
-  loadCity();
-  citySearchList();
-});
-// $(document).on("click", "#searchBtn", citySearchList);
-citySearchList();
+  if (cities.includes(city) === false) {
+    cities.push(city);
+    saveCity(city);  
+  }
+    citySearchList();
+    weatherToday(city);
+    forecastDeck(city);
+}
 
+// Today's weather block
+function weatherToday(city) {
+  var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=bf44d6b9da075580825f81e2aa54cf78";
+  console.log(queryURL);
 
-
-// TODAY
-function weather() {
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=bf44d6b9da075580825f81e2aa54cf78";
-console.log(queryURL);
-
-$.ajax({
-  url: queryURL,
-  method: "GET",
-}).then(function (response) {
-    
-    $("#name").text(response.name + " (" + date +  ")");
-    $("#icon").text(response.weather[0].icon)
-    $("#temp").text("Temperature: " + response.main.temp + " \u00B0F");
-    $("#humidity").text("Humidity: " + response.main.humidity + "%");
-    $("#windSpeed").text("Wind Speed: " + response.wind.speed + " MPH");
-    $("#uvIndex").text("UV Index: " + response.main.temp);
-    
-    console.log(response);
-    
-    console.log(response.weather[0].icon);
-    console.log(response.name);
-    console.log(response.main.temp);
-    console.log(response.main.humidity);
-    console.log(response.wind.speed);
-    console.log(response.main.temp);
-});
-};
-
-
-
-// 05-Day Forecast
-function forecast() {
-var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=bf44d6b9da075580825f81e2aa54cf78";
-console.log(queryURL);
-
-$.ajax({
+  $.ajax({
     url: queryURL,
     method: "GET",
   }).then(function (response) {
     console.log(response);
+    var date = moment.unix(response.dt).format("MM/DD/YYYY");
+    var name = response.name;
+    var icon = response.weather[0].icon;
+    var iconURL = "http://openweathermap.org/img/w/" + icon + ".png";
+    var temp = response.main.temp;
+    var humidity = response.main.humidity;
+    var windSpeed = response.wind.speed;
+    var uvIndex = "";
+    $("#today").append(
+    `<div id="today" class="row">
+      <h2 id="name">City (Date) Icon</h2>
+      <p id="temp">Temp</p>
+      <p id="humidity">Humidity</p>
+      <p id="windSpeed">Wind Speed</p>
+      <p id="uvIndex">UV INdex</p>
+    </div>`
+    )
+  });
+}
 
-    $("#d01").text(response.list[1].dt_txt);
-    $("#t01").text("Temp: " + response.list[1].main.temp + " \u00B0F");
-    $("#i01").text(response.list[1].weather[0].icon);
-    $("#h01").text("Humidity: " + response.list[1].main.humidity + "%");
-    console.log(response.list[1].dt_txt);
-    console.log(response.list[1].weather[0].icon);
-    console.log(response.list[1].main.temp);
-    console.log(response.list[1].main.humidity);
+// Dynamically building the weatherToday section
+// function showWeatherToday(name, date, icon, temp, humidity, windSpeed, uvIndex) {
+//   $("#today").empty();
 
+//   var newH2 = $("<h2>").attr("id", "name").text(name + " (" + date + ") ");
+//   $("<img>").attr("id", "icon").attr("src", icon).attr("alt", "todays weather icon").appendTo(newH2);
+
+//   var newP1 = $("<p>").attr("id", "temp").text("Temperature: " + temp + " \u00B0F");
+//   var newP2 = $("<p>").attr("id", "humidity").text("Humidity: " + humidity + "%");
+//   var newP3 = $("<p>").attr("id", "windSpeed").text("Wind SPeed: " + windSpeed + " MPH");
+//   var newP4 = $("<p>").attr("id", "uvIndex").text("UV Index: " + uvIndex);
+//   $("#today").append(newH2, newP1, newP2, newP3, newP4);
+// }
+
+// Dynamically building the 5-Day forecast section
+function forecastDeck(city) {
+  $(".card-deck").empty();
+  var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=bf44d6b9da075580825f81e2aa54cf78";
+  console.log(queryURL);
+
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function (response) {
+    console.log(response);
+    for (var i = 0; i < response.list.length; i++) {
+
+      if (response.list[i].dt_txt[12] === "2") {
+      // console.log(response.list[i]);
+      var cardDate = moment.unix(response.list[i].dt).format("MM/DD/YYYY");
+      var cardIcon = response.list[i].weather[0].icon;
+      var cardIconURL = "http://openweathermap.org/img/w/" + cardIcon + ".png";
+      var cardTemp = response.list[i].main.temp;
+      var cardHum =  response.list[i].main.humidity;
+
+      $(".card-deck").append(
+      `<div class="card bg-primary">
+        <div class="card-body">
+          <h5 class="card-date">${cardDate}</h5>
+          <img class="card-icon" alt="forecast" src="${cardIconURL}">
+          <p class="card-text">Temp: ${cardTemp}</p>
+        <p class="card-text">Humidity: ${cardHum}</p>
+        </div>
+      </div>`);
+      }
+    }
+  });
+}
+
+//
+$("#clearBtn").on("click", function(event){
+  localStorage.clear();
+  location.reload();  
 });
-};
